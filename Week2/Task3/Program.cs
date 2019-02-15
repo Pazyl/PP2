515b95928863bd27fc4bdffad8a00dc9dab7ace0
Pazyl <pazyl.nurbolat01@gmail.com>
2019-02-16 01:56:36 +0600

    6 commit

--- Week2/Task3/Program.cs
+++ Week2/Task3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,35 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            F3();
+
+            Console.ReadLine();
         }
+
+        private static void PrintInfo(FileSystemInfo fsi, int k)
+        {
+            string line = new string(' ', k);
+            line = line + fsi.Name;
+            Console.WriteLine(line);
+
+            if(fsi.GetType() == typeof(DirectoryInfo))
+            {
+                var items = (fsi as DirectoryInfo).GetFileSystemInfos();
+                foreach (var i in items)
+                {
+                    PrintInfo(i, k + 4);
+                }
+            }
+        }
+
+        private static void F3()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Twins\Desktop\tarik");
+            PrintInfo(dir, 1);
+        }
+
+        
+    
     }
+    
 }
